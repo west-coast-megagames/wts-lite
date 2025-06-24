42cdffb1f2e1034dce8ceb32c6b3245e3a54b906
John T. Cleveland <Jay@WorldAnvil.com>
2025-06-24 12:54:08 -0700

    style(client): :lipstick: swapped out favicon for wcm swooshes

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "WCM | Watch the Skies!" },
+    { name: "description", content: "West Coast Megagames Watch the Skies tracker" },
   ];
 }
 
