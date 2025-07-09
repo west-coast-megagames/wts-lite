d9cdf549f971420b83745a21900992e6931c120c
John T. Cleveland <Jay@WorldAnvil.com>
2025-07-09 02:41:29 -0700

    fix(client): :rotating_light: removed react hydration warning

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -30,14 +30,14 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning={true}>
         <Provider>
           <div className="monitor">
           <TopBar />
