fcbd67cfe2e0fe8096aa9e2f58f18d928ce069b9
Anjineko <4nj1n3k0@gmail.com>
2025-07-08 19:03:19 -0700

    docs: Anji test commit

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
+//hi
 import type { Route } from "./+types/root";
 import "./app.css";
 import { Provider } from "./components/ui/provider";
