659fbd46754c39b0d4b2e0514fb136f658750208
John T. Cleveland <Jay@WorldAnvil.com>
2025-07-11 04:50:00 -0700

    fix(client): :bug: Fixed key bug in tags component

--- client/app/components/molecules/Tags.tsx
+++ client/app/components/molecules/Tags.tsx
@@ -4,7 +4,7 @@ export const Tags = (props: { tags: string[] }) => {
     return (
         <HStack>
         {props.tags.map((tag: string) => 
-            <Tag.Root hideBelow="sm">
+            <Tag.Root key={tag} hideBelow="sm">
                 <Tag.Label>{tag}</Tag.Label>
             </Tag.Root>
         )}
