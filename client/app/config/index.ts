a049d047cb853b8320a0787ed113dcafea783f78
John T. Cleveland <Jay@WorldAnvil.com>
2025-07-18 11:20:43 -0700

    build(client): :green_heart: updated build with deplayed site information

--- client/app/config/index.ts
+++ client/app/config/index.ts
@@ -2,7 +2,7 @@ let server = 'http://localhost:5000/';
 const siteVersion = 'v0.1'
 
 if (process.env.NODE_ENV === "production") {
-	server = 'https://nexus-server.onrender.com/';
+	server = 'https://wts-lite-server.onrender.com/';
 	console.log(process.env);
 }
 
